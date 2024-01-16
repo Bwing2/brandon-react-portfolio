afe6cfe0a0f7b691990bbc923e537ffe95e6e636
Brandon Wing <brandon.wing245@gmail.com>
2024-01-16 11:54:38 -0700

    Reformatted all of the jsx files, and added placeholders to pages.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
-
 function Header() {
-  return(
+  return (
     <header>
       <h1>HELLO! This is a test.</h1>
     </header>
-  ); 
+  );
 }
 
 export default Header;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,38 +1,30 @@
 import { Link, useLocation } from 'react-router-dom';
 
 function Navigation() {
-    {/* Used to get path of current url */}
-    const currentPage = useLocation().pathname;
+  {
+    /* Used to get path of current url */
+  }
+  const currentPage = useLocation().pathname;
 
-    return (
-        <ul>
-            <li>
-                <Link to='/'>
-                    Home
-                </Link>
-            </li>
-            <li>
-                <Link to='/about'>
-                    About
-                </Link>
-            </li>
-            <li>
-                <Link to='/projects'>
-                    Projects
-                </Link>
-            </li>
-            <li>
-                <Link to='/resume'>
-                    Resume
-                </Link>
-            </li>
-            <li>
-                <Link to='/contact'>
-                    Contact
-                </Link>
-            </li>
-        </ul>
-    );
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/About">About</Link>
+      </li>
+      <li>
+        <Link to="/Projects">Projects</Link>
+      </li>
+      <li>
+        <Link to="/Resume">Resume</Link>
+      </li>
+      <li>
+        <Link to="/Contact">Contact</Link>
+      </li>
+    </ul>
+  );
 }
 
 export default Navigation;
