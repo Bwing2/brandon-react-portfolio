234295fe817e4cefa540392e027de2d12ea3a19a
Brandon Wing <brandon.wing245@gmail.com>
2024-01-18 16:10:55 -0700

    Added routes to main jsx file, installed bootstrap npm package.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
-  return <Header />;
+  return (
+    // Wrapped inside React fragment, can return multiple elements from a component.
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,46 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App.jsx';
+import About from './pages/About';
+import Contact from './pages/Contact.jsx';
+import Home from './pages/Home.jsx';
+import Projects from './pages/Projects.jsx';
+import Resume from './pages/Resume.jsx';
+
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/projects',
+        element: <Projects />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
