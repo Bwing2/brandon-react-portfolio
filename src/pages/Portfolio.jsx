1b327a9d014281dc661282cbebbf708dcba540e5
Brandon Wing <brandon.wing245@gmail.com>
2024-01-21 17:01:27 -0700

    Added portfolio card and data pages, adjusted Portfolio page and main jsx.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,13 @@
+import PortfolioCard from '../components/portfolio/portfolioCard';
+import { portfolioData } from '../components/portfolio/portfolioData';
+
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>Placeholder Text Portfolio.</p>
+      {portfolioData.map((project, index) => (
+        <PortfolioCard key={index} project={project} />
+      ))}
     </div>
   );
 }
