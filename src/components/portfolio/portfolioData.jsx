4ce4efc3d3a5a1f25511a443710b5c37849dec88
Brandon Wing <brandon.wing245@gmail.com>
2024-01-22 17:20:57 -0700

    Added more projects and images to portfolio page.

--- src/components/portfolio/portfolioData.jsx
+++ src/components/portfolio/portfolioData.jsx
@@ -1,7 +1,9 @@
 import project0 from '../../images/sat_seeker.png';
 import project1 from '../../images/dota_api_website.png';
-import project2 from '../../images/rocket_launch_info.png';
-import project3 from '../../images/weather_app.png';
+import project2 from '../../images/mvc_blog_homepage.png';
+import project3 from '../../images/rocket_launch_info.png';
+import project4 from '../../images/get_route_nosql.png';
+import project5 from '../../images/weather_app.png';
 
 export const portfolioData = [
   {
@@ -25,6 +27,16 @@ export const portfolioData = [
     imgAlt: 'Dota Longest and Shortest Game Stats Search Page',
   },
   {
+    name: 'Cool Ideas Blog',
+    description:
+      'A blog application using the Model View Controller (MVC) paradigm to create and organize a blog site.',
+    hrefGithub: 'https://github.com/Bwing2/brandon-mvc-tech-blog',
+    hrefDeployed: 'https://brandon-mvc-tech-blog-cc4f8f869b6d.herokuapp.com/',
+    languages: ['Javascript', 'CSS', 'Node.js', 'Express.js'],
+    imgSrc: project2,
+    imgAlt: 'Cool Ideas Blog Home Page',
+  },
+  {
     name: 'Rocket Launches and News Database',
     description:
       'A web app that will allow you to search current, past, or upcoming launches. Also includes up-to-date space news. ',
@@ -33,8 +45,18 @@ export const portfolioData = [
     hrefDeployed:
       'https://sparkkids.github.io/rocket-launches-and-news-database-group-project/',
     languages: ['HTML', 'CSS', 'Tailwind', 'Javascript'],
-    imgSrc: project2,
+    imgSrc: project3,
     imgAlt: 'Rocket Launches and News Database Search Page',
+  },
+  {
+    name: 'NoSQL Social Network API',
+    description:
+      'A backend application that uses NoSQL to build the database for a social network API. (Video demonstration included below.)',
+    hrefGithub: 'https://github.com/Bwing2/brandon-social-network-api',
+    hrefDeployed: 'https://www.youtube.com/watch?v=1LeIPhDVkdw',
+    languages: ['Javascript', 'Node.js', 'Express.js ,', 'Mongoose', 'MongoDB'],
+    imgSrc: project4,
+    imgAlt: 'Insomnia GET Route for NoSQL Social Network Api',
   },
   {
     name: 'Weather Dashboard',
@@ -45,7 +67,7 @@ export const portfolioData = [
     hrefDeployed:
       'https://bwing2.github.io/brandon-weather-dashboard-challenge-6/',
     languages: ['HTML', 'CSS', 'Javascript', 'Server-Side API'],
-    imgSrc: project3,
+    imgSrc: project5,
     imgAlt: 'Weather Dashboard Search Page',
   },
 ];
