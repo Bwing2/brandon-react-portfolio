59a97afcc13792258b05c17ab444658427574a55
Brandon Wing <brandon.wing245@gmail.com>
2024-02-14 10:49:10 -0700

    Updated resume and links.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,12 @@
-import resume1 from "../images/updated_resume1.png";
-import resume2 from "../images/updated_resume2.png";
+import resume1 from "../images/updated_resume1.jpg";
+import resume2 from "../images/updated_resume2.jpg";
 
 export default function Resume() {
   return (
     <div className="resume-container">
       <h1>Resume</h1>
       <a
-        href="https://drive.google.com/file/d/1XzM6frkWGmWvREg7dCH1rdBRsF9zRSaO/view?usp=sharing"
+        href="https://drive.google.com/file/d/1GqCki3tQ4NzJ0YpdtWAxxf8BRcbjw-al/view?usp=sharing"
         className="btn custom-button-color resume-download"
       >
         Link to Download
